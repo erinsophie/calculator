c06a101391b591bff83429d56ee4999bea4bb3bd
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-02-17 17:16:19 +0000

    Create scientific notation calculation

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const previousDisplay = document.querySelector('[data-previous-operand]');
 
 // EVENT LISTENERS
 
+
 numberBtn.forEach(button => button.addEventListener('click', () => appendNumber(button.textContent)));
 operatorBtn.forEach(button => button.addEventListener('click', () => setOperation(button.textContent)));
 clearBtn.addEventListener('click', clear);
@@ -18,13 +19,15 @@ equalsBtn.addEventListener('click', evaluate);
 
 //KEEP TRACK OF THE STATE OF THE CALCULATOR WITH CALCULATOR OBJECT 
 
+
 const calculator = {
     currentOperand: '0',
     previousOperand: '',
     operation: undefined,
 };
 
-// UPDATE THE DISPLAY   
+// UPDATE THE DISPLAY 
+
 
 function updateDisplay() {
     currentDisplay.textContent = calculator.currentOperand;
@@ -40,6 +43,7 @@ function appendNumber(number) {
     if(exceedsCurrent()) return;
 
     if (number === "." && calculator.currentOperand.includes(".")) return;
+
     if (calculator.currentOperand === "0" && number !== ".") {
         calculator.currentOperand = number;
     } else {
@@ -52,17 +56,16 @@ function appendNumber(number) {
     updateDisplay();
 };
 
-//LIMIT CURRENT CHARACTER DISPLAY
+//LIMIT CURRENT CHARACTER INPUT 
 
 function exceedsCurrent() {
     if (calculator.currentOperand.length >= 18) {
-        return true
+       return true;
     }
 };
 
-//LIMIT PREVIOUS DISPLAY
-
 //SET OPERATION 
+
 
 function setOperation(operation) {
     calculator.operation = operation;
@@ -73,8 +76,15 @@ function setOperation(operation) {
 
 // CLEAR SCREEN
 
+
 function clear() {
-    calculator.currentOperand = calculator.currentOperand.toString().slice(0, -1);
+    if (calculator.currentOperand === '0') return;
+    if (calculator.currentOperand !== '0') {
+        calculator.currentOperand = calculator.currentOperand.toString().slice(0, -1);
+    }
+    if (calculator.currentOperand.length < 1) {
+        calculator.currentOperand = '0';
+    };
     updateDisplay();
 };
 
@@ -85,9 +95,8 @@ function allClear() {
 };
 
 
+// CALCULATE SUM WHEN EQUALS BUTTON IS CLICKED
 
-
-// CALCULATE SUM WHEN EQUALS BUTTON IS CLICKED 
 
 function evaluate() {
     let result;
@@ -95,13 +104,34 @@ function evaluate() {
     const previous = parseFloat(calculator.previousOperand);
     if (isNaN(current) || isNaN(previous) || !calculator.operation) {
       return;
-    }
+    };
     result = operate(calculator.operation, previous, current);
-    calculator.currentOperand = result.toString();
-    calculator.previousOperand = `${previous} ${calculator.operation} ${current} =`
+    calculator.currentOperand = formatNumber(result);
+    calculator.previousOperand = `${previous} ${calculator.operation} ${current} =`;
     calculator.operation = undefined;
     updateDisplay();
 };
+
+// Display large numbers using scientific notation 
+// if result has more than 10 digits before decimal point convert to scientifc notation which displays 1 digit to 6 decimal places
+// otherwise reduce any other result that is under that threshold but includes a decimal place to 2 decimal places 
+// if there is no decimal in the result then display the result as normal
+
+function formatNumber(number) {
+  const threshold = 1e10; 
+  if (Math.abs(number) >= threshold) {
+    return number.toExponential(6); // 6 decimal places
+  } else {
+     if (number.toString().includes('.')) {
+     return number.toFixed(2); // if result includes decimal point, round to 2 decimal places 
+  } else {
+    return number.toString();
+   }
+  }
+};
+
+
+// OPERATOR FUNCTIONS
 
 
 const operate = (operator, a, b) => {
@@ -119,8 +149,6 @@ const operate = (operator, a, b) => {
     }
 };
 
-
-// OPERATOR FUNCTIONS
 
 const add = (a, b) => {
     return a + b;
